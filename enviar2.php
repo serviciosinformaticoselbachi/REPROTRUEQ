fbd3cf90859b0b2f8266c83dd3e3e41d4ef3c357
serviciosinformaticoselbachi <50127280+serviciosinformaticoselbachi@users.noreply.github.com>
2020-02-02 13:06:02 -0300

    Update enviar2.php

--- enviar2.php
+++ enviar2.php
@@ -1,7 +1,8 @@
 <?php
-$nombre = $_POST['nombre'];
-$mail = $_POST['email'];
-$empresa = $_POST['mensaje'];
+$para = "servicioselbachi@gmail.com";
+$nombre = $_POST["nombre"];
+$mail = $_POST["email"];
+$empresa = $_POST["mensaje"];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -13,8 +14,8 @@ $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'servicioselbachi@gmail.com';
-$asunto = 'Mensaje de mi sitio web';
+
+$asunto = "Mensaje de mi sitio web";
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
 
