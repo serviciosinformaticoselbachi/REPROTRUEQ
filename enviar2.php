096dba6c17e31ca3eb84cab7319e17558ac8a417
serviciosinformaticoselbachi <50127280+serviciosinformaticoselbachi@users.noreply.github.com>
2020-02-02 12:13:56 -0300

    Add files via upload

---
+++ enviar2.php
@@ -0,0 +1,22 @@
+<?php
+$nombre = $_POST['nombre'];
+$mail = $_POST['email'];
+$empresa = $_POST['mensaje'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'ignaciobluuweb@gmail.com';
+$asunto = 'Mensaje de mi sitio web';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:index.html");
+?>
