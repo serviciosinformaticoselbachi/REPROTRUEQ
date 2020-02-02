4fb18739e23485d77da40192984a8f85c37a11c3
serviciosinformaticoselbachi <50127280+serviciosinformaticoselbachi@users.noreply.github.com>
2020-02-02 12:15:48 -0300

    Update enviar2.php

--- enviar2.php
+++ enviar2.php
@@ -13,7 +13,7 @@ $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'ignaciobluuweb@gmail.com';
+$para = 'servicioselbachi@gmail.com';
 $asunto = 'Mensaje de mi sitio web';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
