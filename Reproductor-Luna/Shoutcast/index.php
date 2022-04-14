399e15796885bef861ffaa83eeb296902fa45537
serviciosinformaticoselbachi <50127280+serviciosinformaticoselbachi@users.noreply.github.com>
2022-04-14 12:59:09 -0300

    Add files via upload

---
+++ Reproductor-Luna/Shoutcast/index.php
@@ -0,0 +1,41 @@
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script src="js/lunaradio-sincors.js"></script>
+
+<div id="lunaradio" style="width:100%; height:100%;background: #000000 url(js/portada.jpg) bottom / cover no-repeat;">loading...</div>			
+
+<script>
+$("#lunaradio").lunaradio({
+userinterface: "big", 
+backgroundcolor: "",
+fontcolor: "#ffffff",
+hightlightcolor: "#FF6767", 
+fontname: "Unica One", 
+googlefont: "Unica+One&display=swap", 
+fontratio: "0.4", 
+radioname: "EmpresarialHost.com", 
+scroll: "true", 
+coverimage: "js/logo.jpg",
+onlycoverimage: "false",
+coverstyle: "animated",
+usevisualizer: "real", 
+visualizertype: "4", 
+metadatatechnic: "",
+ownmetadataurl: "", 
+streamurl: "https://radioenhd.com:7121", 
+streamtype: "shoutcast2", 
+icecastmountpoint: "",
+shoutcastpath: "/stream",
+shoutcastid: "1", 
+streamsuffix: "", 
+radionomyid: "", 
+radionomyapikey: "",
+radiojarid: "",  
+radiocoid: "",
+itunestoken: "1000lIPN", 
+metadatainterval: "5000", 
+volume: "100",
+debug: "false",
+autoplay: "true"
+});
+</script>
